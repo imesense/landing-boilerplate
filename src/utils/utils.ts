2334a00c4b64cf540086b3e920bfa55985e6ebf0
Z3SA <silver.lets-go@yandex.ru>
2020-10-26 19:08:57 +0800

    feat(utils): added date formatter

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
-import pick from 'lodash/pick';
+import padStart from 'lodash-es/padStart';
+import pick from 'lodash-es/pick';
 
 export default class Utils {
   static DEVICE_WIDTHS = {
@@ -20,5 +21,19 @@ export default class Utils {
     }
 
     return data;
+  }
+
+  static formatDate(date: string | Date, format: 'dd.mm.yyyy'): string {
+    const currentDate = typeof date === 'string' ? new Date(date) : date;
+
+    if (format === 'dd.mm.yyyy') {
+      return `${
+        padStart(currentDate.getDate().toString(), 2, '0')
+      }.${
+        padStart((currentDate.getMonth() + 1).toString(), 2, '0')
+      }.${
+        currentDate.getFullYear().toString()
+      }`;
+    }
   }
 }
