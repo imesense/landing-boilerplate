36854a988219b5383f5a0a2558b79cae1540e81f
Z3SA <silver.lets-go@yandex.ru>
2020-10-26 19:53:02 +0800

    feat(components): added refresh of main page on click on logo

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,4 +1,5 @@
 import styles from './Logo.module.scss';
+import { useRouter } from 'next/router';
 
 interface LogoProps {
   width?: number;
@@ -8,10 +9,13 @@ interface LogoProps {
 }
 
 export default function Logo({ width, height, alt, className }: LogoProps) {
+  const router = useRouter();
+
   return (
     <span 
       className={`${styles.Logo} ${alt ? styles.Logo_alt : ''} ${className || ''}`.trim()} 
-      style={{ width, height }} 
+      style={{ width, height }}
+      onClick={() => router.push('/')}
     />
   );
 }
