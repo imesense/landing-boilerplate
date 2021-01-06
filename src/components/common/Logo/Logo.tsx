cdd515d51871bf3d0acbd1f1ee16042713fbb8e4
Nikita Bukin <me@z3sa.ru>
2021-01-06 14:21:04 +0800

    feat(common): added routing in logo

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,15 +1,24 @@
 import styles from './Logo.module.scss';
+import { useRouter } from 'next/router';
 
 interface LogoProps {
   width?: number;
   height?: number;
   alt?: boolean;
   className?: string;
+  big?: boolean;
 }
 
-export const Logo = ({ width, height, alt, className }: LogoProps) => (
-  <span 
-    className={`${styles.Logo} ${alt ? styles.Logo_alt : ''} ${className || ''}`.trim()} 
-    style={{ width, height }} 
-  />
-);
+export const Logo = ({ width, height, alt, className, big }: LogoProps) => {
+  const router = useRouter();
+
+  return (
+    <span 
+      className={
+        `${styles.Logo} ${alt ? styles.Logo_alt : ''} ${className || ''} ${big ? styles.Logo_big : ''}`.trim()
+      } 
+      style={{ width, height }}
+      onClick={() => router.push('/')}
+    />
+  );
+}
