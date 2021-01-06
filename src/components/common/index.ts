109c0dbb8fd0191ef02add88f45d51caf4553cd1
Nikita Bukin <me@z3sa.ru>
2021-01-06 14:06:39 +0800

    feat: added breadcrumbs component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,11 +1,13 @@
-import Logo from './Logo/Logo';
 import Section from './Section/Section';
 import Col from './Col/Col';
 import PhoneLink from './PhoneLink/PhoneLink';
 import PolicyLink from './Policy/PolicyLink';
 
+export { Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
+export { Logo } from './Logo/Logo';
+
 export * from './Form';
 export * from './Slider';
 export * from './Modal';
 export * from './Button';
-export { Logo, Section, Col, PhoneLink, PolicyLink };
+export { Section, Col, PhoneLink, PolicyLink };
